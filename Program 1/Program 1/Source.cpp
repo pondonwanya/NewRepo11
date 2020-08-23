5d72ddf810c5363260e4510941ee1e8042c0f5d1
pondonwanya <63011060@kmitl.ac.th>
2020-08-23 22:37:12 +0700

    add commit

--- Program 1/Program 1/Source.cpp
+++ Program 1/Program 1/Source.cpp
@@ -6,13 +6,22 @@ int main()
 	printf("Enter number :");
 	scanf("%d", &num);
 	printf("Factoring Result : ");
-	while ((num > 1) && (a < num)) {
-		if (num % a == 0) {
-			printf("%d x ", a);
-			num = num / a;
-		}
-		else a = a + 1;
+	if (num < 0) {
+		num = num * (-1);
+		printf("-1 x ");
 	}
-	printf("%d\n", a);
+	if ((num == 1) || (num == 0)) {
+		printf("%d", num);
+	}
+	else {
+		while ((num > 1) && (a < num)) {
+			if (num % a == 0) {
+				printf("%d x ", a);
+				num = num / a;
+			}
+			else a = a + 1;
+		}
+		printf("%d\n", a);
+	}
 	return 0;
 }
